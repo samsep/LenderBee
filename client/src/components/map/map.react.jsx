c5deb2ca5d63a4012b9ea8d7c36059cacfef8689
sampsep <saminsepasi@gmail.com>
2015-02-27 12:17:18 -0800

    (feat) tooltips on map

--- client/src/components/map/map.react.jsx
+++ client/src/components/map/map.react.jsx
@@ -11,7 +11,32 @@ var map = React.createClass({
 	mixins: [Reflux.connect(mapStore)],
 
   // [Tip] Invoked once immediately after initial rendering, has DOM rep with this.getDOMNode()
+
+
   componentDidMount: function() {
+
+    // var boxText = document.createElement("div");
+    //   boxText.style.cssText = "margin-top: 1px; background: yellow; padding: 1px;";
+    //   boxText.innerHTML = "City Hall, Sechelt<br>British Columbia<br>Canada";
+
+    // var windowOptions = {
+    //    content: boxText
+    //   ,disableAutoPan: false
+    //   ,maxWidth: 100
+    //   ,pixelOffset: new google.maps.Size(-140, 0)
+    //   ,zIndex: 5
+    //   ,boxStyle: { 
+    //     opacity: 0.75
+    //     ,width: "280px"
+    //    }
+    //   ,infoBoxClearance: new google.maps.Size(-16, -11)
+    //   ,isHidden: false
+    //   ,pane: "floatPane"
+    //   ,enableEventPropagation: false
+    // };
+        
+    //   var infoBox = new InfoBox(windowOptions);
+
       $(".carousel").remove();
       var mapOptions = {center: new google.maps.LatLng(37.7836245,-122.4089988), zoom: 13};
       var gMap = new google.maps.Map(this.getDOMNode(), mapOptions);
